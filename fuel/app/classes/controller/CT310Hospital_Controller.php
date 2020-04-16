8109b64fe535b60f3cb0fa07b7e16c7d23e903d8
Maddie <maddie@mihevc.com>
2020-04-15 22:48:05 -0600

    Adding fuel and everything from brandts email

---
+++ fuel/app/classes/controller/CT310Hospital_Controller.php
@@ -0,0 +1,38 @@
+<?php
+
+class CT310Hospital_Controller extends Controller {
+
+    //TODO: directs you to the homepage (serve up the homepage view)
+    public function action_index() {
+
+    }
+
+    //TODO: directs you to the about page (serve up the about page view)
+    public function action_about() {
+
+    }
+
+    //TODO: directs you to a page listing the hospitals, it would be smart to limit the data flow using pagination or something like that...
+    //https://www.myprogrammingtutorials.com/create-pagination-with-php-and-mysql.html
+    //You could do something like this with a file, but you could also just load all the data into a database for easier access
+    public function action_hospital_list() {
+
+    }
+
+    //TODO: directs you to a page listing the msdrgs, maybe do some pagination similar to the hospital list
+    //Something similar to the hospital list view
+    public function action_msdrg_list() {
+
+    }
+
+    //TODO: serves up the view with the details for a specific hospital
+    public function action_hospital_details($hospital_id) {
+
+    }
+
+    //TODO: serves up a view with the details for a specific msdrg
+    public function action_msrg_details($msdrg_id) {
+
+    }
+}
+
