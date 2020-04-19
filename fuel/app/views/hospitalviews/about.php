1d9529eec96a45438f48f6247a83b91f63f13aac
Jclotfel101 <60152594+Jclotfel101@users.noreply.github.com>
2020-04-19 14:10:35 -0600

    Added jackies info to about page

--- fuel/app/views/hospitalviews/about.php
+++ fuel/app/views/hospitalviews/about.php
@@ -18,14 +18,22 @@
           has learned as a TA become a CS teacher after she graduates. She is very
           passionate about getting girls involved in computer science and hopes to get more
           girls involved as a teacher. She enjoys long walks with her dog Razmataz and
-          participating in any outdoor adventure possible. 
+          participating in any outdoor adventure possible.
         </td>
         <td class="link">deployment link</td>
       </tr>
       <tr>
         <th>Jackie Clotfelter</th>
-        <td>image here</td>
-        <td>bio here</td>
+        <td><img src="https://www.cs.colostate.edu:4444/~jclotfel/ct310/assets/img/jackie.png"></td>
+        <td>Jackie is a senior at Colorado State University majoring in Applied Computing Technology.
+          She believes when we support each other we can accomplish great things and thrives in team
+          settings. Coming into the major with little to no experience in the field, her knowledge
+          of computer science is constantly growing as she is eager to learn. She has experience working
+          in an industry setting with two summer internships at Webroot Cyber Security where she interned
+          under a product owner working with Salesforce and Oracle teams. Originally born in the bay area
+          of California, her family moved to Colorado when she was a young age to pursue their love for
+          the skiing and the outdoors. She loves plants, pugs, and painting and despises being cold.
+        </td>
         <td class="link">deployment link</td>
       </tr>
       <tr>
