89d0d6e188ec5ba8e34274efbec6d5d448ec6ad9
mmihevc <34844604+mmihevc@users.noreply.github.com>
2020-04-19 00:34:10 -0600

    Create template.php

---
+++ fuel/app/views/hospitalviews/template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <?php echo Asset::css('hospital.css') ?>
+    <title></title>
+  </head>
+  <body>
+    <h1>Professional People Fixers</h1>
+    <div class="topnav">
+      <a href="home.php">Home</a>
+      <a href="about.php">About Us</a>
+      <a href="hospitalList.php">Hospital List</a>
+      <a href="drgList.php">DRG List</a>
+      <a href="hospitalDetails.php">Hospital Details</a>
+      <a href="drgDetails.php"></a>
+    </div>
+
+  </body>
+</html>
