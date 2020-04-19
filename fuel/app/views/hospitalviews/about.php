d1bac4575692876c6f7e31a8eea46f8613a89c2a
mmihevc <34844604+mmihevc@users.noreply.github.com>
2020-04-19 13:38:45 -0600

    Update about.php

---
+++ fuel/app/views/hospitalviews/about.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $title ?></title>
+  </head>
+  <body>
+    <?php echo $contents ?>
+    <br><br>
+    <h1>Read about who we are</h1>
+    <table>
+      <tr>
+        <th>Maddie Mihevc</th>
+        <td><img src="https://www.cs.colostate.edu:4444/~mmihevc/ct310/assets/img/maddie.png?1587324047"></td>
+        <td>
+          Maddie Mihevc is majoring in Applied Computing Technology and plans on graduating
+          in the fall. She is currently the lead TA for CS150 and hopes to take what she
+          has learned as a TA become a CS teacher after she graduates. She is very
+          passionate about getting girls involved in computer science and hopes to get more
+          girls involved as a teacher. She enjoys long walks with her dog Razmataz and
+          participating in any outdoor adventure possible. 
+        </td>
+        <td class="link">deployment link</td>
+      </tr>
+      <tr>
+        <th>Jackie Clotfelter</th>
+        <td>image here</td>
+        <td>bio here</td>
+        <td class="link">deployment link</td>
+      </tr>
+      <tr>
+        <th>Arif Biyik</th>
+        <td>image here</td>
+        <td>bio here</td>
+        <td class="link">deployment link</td>
+      </tr>
+    </table>
+
+  </body>
+</html>
