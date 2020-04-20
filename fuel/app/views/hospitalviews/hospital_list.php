123e383b87215600f73271eb59756aa6fb59f840
Jclotfel101 <60152594+Jclotfel101@users.noreply.github.com>
2020-04-20 12:20:36 -0600

    Added hospital_list.php
    has a lot of commented out code while i try to figure out my server issue but can be un commented

---
+++ fuel/app/views/hospitalviews/hospital_list.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $title ?></title>
+  </head>
+  <body>
+    <?php echo $contents ?>
+    <!-- <div class="row">
+      <div class ="col-12">
+        <table id="hospitalTable" class="table tablesorter">
+          <thread class ="thread-dark">
+            <tr>
+              <th>Name</th>
+              <th>State</th>
+              <th>MPN</th<
+
+            </tr>
+            </thread>
+            <tbody>
+              <?php
+              foreach($hospital_data as $row){
+                echo "<tr>\n";
+                foreach ($row as $item)
+                  echo "<td>" . $item . "</td>\n";
+              }
+              echo "</tr>\n";
+            }
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div> -->
+
+    <img src="https://www.cs.colostate.edu:4444/~mmihevc/ct310/assets/img/logo.png?1587274978" alt="">
+
+  </body>
+</html>
