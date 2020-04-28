8c25c99feef08207670317ab52b62be4c7838261
mmihevc <34844604+mmihevc@users.noreply.github.com>
2020-04-28 16:32:50 -0600

    Update hospital_details.php

--- fuel/app/views/hospitalviews/hospital_details.php
+++ fuel/app/views/hospitalviews/hospital_details.php
@@ -19,16 +19,6 @@
               <th>Average Cover Charges</th>
               <th>Average Total Payments</th>
               <th>Average Medicare Payments</th>
-
-              <!-- <th>DRG Definition</th>
-              <th>Street Address</th>
-              <th>City<th>
-              <th>Zip</th>
-              <th>Referral Decription</th>
-              <th>Total Discharges</th>
-              <th>Average Covered Charges</th>
-              <th>Total Payments on Average</th>
-              <th>Average Medicare Payments</th> -->
             </tr>
             </thead>
             <tbody>
@@ -38,7 +28,7 @@
               foreach($everything_data as $row){
                 echo "<tr>\n";
                 // $l = Uri::base(). "ct310/index/hospital/hospital_details/" .$row["Provider_Name"];
-                $data = explode('-', $row["DRG_Definition"]);
+                $data = explode(' - ', $row["DRG_Definition"]);
                 $number = trim($data[0]); //number
                 $description = trim($data[1]); //description
                 $drg = Uri::base(). "index.php/hospital/drg_details.php?drg=" .$number;
@@ -47,16 +37,8 @@
                 echo "<td>" . $row["Average_Covered_Charges"] . "</td>";
                 echo "<td>" . $row["Average_Total_Payments"] . "</td>";
                 echo "<td>" . $row["Average_Medicare_Payments"] . "</td>";
-                // echo "<td>" . $row["DRG_Definition"] . "</td>";
-                // echo "<td>" . $row["Provider_Street_Address"] . "</td>";
-                // echo "<td>" . $row["Provider_City"] . "</td>";
-                // echo "<td>" . $row["Provider_Zip_Code"] . "</td>";
-                // echo "<td>" . $row["Hospital_Referral_Region_HRR_Description"] . "</td>";
-                // echo "<td>" . $row["Total_Discharges"] . "</td>";
 
-
-
-              echo "</tr>\n";
+                echo "</tr>\n";
             }
           //}
             ?>
