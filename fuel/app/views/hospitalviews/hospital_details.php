e0f791df1008446ac71c42be28b8b729474760f9
mmihevc <34844604+mmihevc@users.noreply.github.com>
2020-05-04 11:15:55 -0600

    Update hospital_details.php

--- fuel/app/views/hospitalviews/hospital_details.php
+++ fuel/app/views/hospitalviews/hospital_details.php
@@ -24,27 +24,41 @@
             <tbody>
 
               <?php
-              // if(isset($everything_data) ){
-              foreach($everything_data as $row){
-                echo "<tr>\n";
-                // $l = Uri::base(). "ct310/index/hospital/hospital_details/" .$row["Provider_Name"];
-                $data = explode(' - ', $row["DRG_Definition"]);
-                $number = trim($data[0]); //number
-                $description = trim($data[1]); //description
-                $drg = Uri::base(). "index.php/hospital/msdrg_details.php?msdrg_id=" .$number;
-                echo "<td><a href ='$drg'>" . $number . "</a></td>";
-                echo "<td>" . $description . "</td>";
-                echo "<td>" . $row["Average_Covered_Charges"] . "</td>";
-                echo "<td>" . $row["Average_Total_Payments"] . "</td>";
-                echo "<td>" . $row["Average_Medicare_Payments"] . "</td>";
-
-                echo "</tr>\n";
-            }
-          //}
+              //if(isset($everything_data) ){
+                foreach($everything_data as $row){
+                  echo "<tr>\n";
+                  $data = explode(' - ', $row["DRG_Definition"]);
+                  $number = trim($data[0]); //number
+                  $description = trim($data[1]); //description
+                  $drg = Uri::base(). "index.php/hospital/msdrg_details.php?msdrg_id=" .$number;
+                  echo "<td><a href ='$drg'>" . $number . "</a></td>";
+                  echo "<td>" . $description . "</td>";
+                  echo "<td>" . $row["Average_Covered_Charges"] . "</td>";
+                  echo "<td>" . $row["Average_Total_Payments"] . "</td>";
+                  echo "<td>" . $row["Average_Medicare_Payments"] . "</td>";
+                  echo "</tr>\n";
+              }
+            //}
             ?>
           </tbody>
         </table>
       </div>
     </div>
+
+    <nav label="navigation">
+      <ul class="list justify-content-sm-center">
+        <?php
+          if ($start > 0) {
+            $prev_path = Uri::base() . 'index.php/hospital/hospital_details?start=' . max($start - 20, 0);
+            echo '<li class="page-item"><a class="page-link" href="' . $prev_path . '">Previous</a></li>';
+          }
+         ?>
+         <?php
+            $next_path = Uri::base() . 'index.php/hospital/hospital_details?start=' . ($start + 20);
+            echo '<li class="page-item"><a class="page-link" href="' . $next_path . '">Next</a></li>';
+          ?>
+      </ul>
+    </nav>
+
   </body>
 </html>
